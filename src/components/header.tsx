8eeeddde0828d8f4d4d9d5b75dc1d374257a91db
overhead525 <ritchelcousar@outlook.com>
2020-10-05 11:08:39 -0400

    Got a good skeleton

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,41 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+import * as React from "react";
+import { Link } from "gatsby";
 
 type Props = {
-  siteTitle: string;
+    siteTitle: string;
 };
 
 const Header: React.FC<Props> = ({ siteTitle }: Props) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <header
+        style={{
+            background: `hsl(204, 86%, 53%)`,
+            marginBottom: `1.45rem`,
+        }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+        <div
+            style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `1.45rem 1.0875rem`,
+            }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+            <h1 style={{ margin: 0 }}>
+                <Link
+                    to="/"
+                    style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                    }}
+                >
+                    {siteTitle}
+                </Link>
+            </h1>
+        </div>
+    </header>
 );
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 };
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,64 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
+import "bulma/css/bulma.min.css";
+
 const IndexPage: React.FC = () => (
     <Layout>
         <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div
-            style={{
-                maxWidth: `300px`,
-                marginBottom: `1.45rem`,
-            }}
-        >
-            <Image />
+        <section className="hero">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">
+                        Learn About the <strong>Risk Aversion</strong> Project
+                    </h1>
+                </div>
+            </div>
+        </section>
+        <div className="columns">
+            <aside className="menu column is-one-fifth">
+                <p className="menu-label">Motivation</p>
+                <ul className="menu-list">
+                    <li>
+                        <a>Why Build This?</a>
+                    </li>
+                    <li>
+                        <a>Who Uses Risk Aversion?</a>
+                    </li>
+                </ul>
+                <p className="menu-label">Architecture</p>
+                <ul className="menu-list">
+                    <li>
+                        <a>Bird's Eye View</a>
+                    </li>
+                    <li>
+                        <a>Details</a>
+                        <ul>
+                            <li>
+                                <a>Simulation Core</a>
+                            </li>
+                            <li>
+                                <a>Simulation Request API</a>
+                            </li>
+                            <li>
+                                <a>Web Application Backend</a>
+                            </li>
+                            <li>
+                                <a>User Interface</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <p className="menu-label">Engineering Highlights</p>
+                <ul className="menu-list">
+                    <li>
+                        <a>Coming Soon...</a>
+                    </li>
+                </ul>
+            </aside>
+            <div className="column is-four-fifths">
+                <h1>Hello</h1>
+            </div>
         </div>
         <ul>
             <li>
